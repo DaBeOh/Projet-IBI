dcd7901bdba264ea359a9b36c8c575bedc7291c5
dabeoh <hugo.mouden@gmail.com>
2021-02-15 13:50:32 +0100

    update script partie 2

--- nettoyage_bwa.sh
+++ nettoyage_bwa.sh
@@ -4,7 +4,7 @@ i=0
 for fastqfile in *.fastq.gz
 do
 	file=${fastqfile%%.*}
-	bwa mem -R '@RG\tID:'S288C'\tPL:ILLUMINA\tPI:0\tSM:'[le_nom_du_sample]'\tLB:1' ${ref} ${fastqfile} > bwa_${file}.sam
+	bwa mem -R '@RG\tID:'S288C'\tPL:ILLUMINA\tPI:0\tSM:'${file}'\tLB:1' ${ref} ${fastqfile} > bwa_${file}.sam
 	samtools view -bS bwa_${file}.sam > bwa_${file}.bam
 	gatk MarkDuplicatesSpark \
 			-I bwa_${file}.bam \
