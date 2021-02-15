5f67cd55502ec842b6d861d7189a53f90f523f01
dabeoh <hugo.mouden@gmail.com>
2021-02-15 13:13:11 +0100

    adding gatk

--- bwa_test.sh
+++ bwa_test.sh
@@ -1,6 +1,13 @@
 ref=S288C_reference_sequence_R64-2-1_20150113.fsa
 bwa index ${ref}
+i=0
 for fastqfile in *.fastq.gz
 do
-	bwa mem ${ref} ${fastqfile} > bwa_mem_test_1.sam
+	file=${fastqfile%%.*}
+	bwa mem -R '@RG\tID:'S288C'\tPL:ILLUMINA\tPI:0\tSM:'[le_nom_du_sample]'\tLB:1' ${ref} ${fastqfile} > bwa_${file}.sam
+	samtools view -bS bwa_${file}.sam > bwa_${file}.bam
+	gatk MarkDuplicatesSpark \
+			-I bwa_${file}.bam \
+            -O marked_duplicates_${file}.bam
 done
+
