12ea2a5f44226bdb74e1f05c6d519e23b8c310b5
Hugo Mouden <hugo.mouden@gmail.com>
2021-04-05 23:43:49 +0200

    corrections légères filtration

--- pipeline_gatK/mep.sh
+++ pipeline_gatK/mep.sh
@@ -6,17 +6,23 @@ cd Hard_Filtering
 ## 2- Filtrer avec les outils de la suite GATK:
 ## permet de sélectionner uniquement les variants de SNP:
 
-gatk -T SelectVariants -R ../../genome/fichier.fa 
--V ../../part2_var_calling/fichier.vcf -selectType SNP -o fichier.vcf
+gatk SelectVariants \
+		-R ../../genome/fichier.fa \
+	 	-V ../../part2_var_calling/fichier.vcf \
+	 	-selectType SNP \
+	 	-O fichier.vcf
 
 
 ##Filtrer nos SNP selon les paramètres de seuils recommandés par GATK:
 
 # Bos_taurus.UMD3.1.dna.toplevel.6.fa -- 
-gatk -T VariantFiltration -R ../../genome/fichier.fa 
--V fichier.vcf --filterExpression "QD < 2.0 || FS > 60.0 ||
- MQ < 40.0 || MQRankSum < -12.5 || ReadPosRankSum < -8.0" 
- --filterName "hard_filtering_snp" -o fichier.vcf
+gatk VariantFiltration \
+	 	-R ../../genome/fichier.fa \
+	 	-V fichier.vcf \
+	 	--filterExpression "QD < 2.0 || FS > 60.0 || MQ < 40.0 || 
+	 					 	MQRankSum < -12.5 || ReadPosRankSum < -8.0" \
+ 		--filterName "hard_filtering_snp" \
+ 		-O fichier.vcf
 
 
 
@@ -24,9 +30,11 @@ gatk -T VariantFiltration -R ../../genome/fichier.fa
 # Cela va nous permettre de garder uniquement les variants qui ont 
 # le tag 'PASS' dans le champ 'FILTER' de notre fichier vcf.
 
-gatk -T SelectVariants -R ../../genome/fichier.fa 
--V fichier.vcf -o fichier.vcf 
---excludeFiltered
+gatk SelectVariants \
+		-R ../../genome/fichier.fa \
+		-V fichier.vcf \
+		-O fichier.vcf \
+		--excludeFiltered
 
 
 #------------------------------------------------------SNPSIFT
